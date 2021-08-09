a0ab53b1160d04ec3fcfb79174ce9471d4593af1
“Muhammad <mmuttaki@uoguelph.ca>
2021-08-09 16:11:13 -0400

    Fixed the comm lib

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,12 @@
-import { FETCH_PRODUCTS } from "./type";
-import axios from 'axios';
+import { FETCH_PRODUCTS, FETCH_PRODUCT } from "./type";
 import { commerce } from '../lib/commerce';
 
-export const fetchProducts = () => {
-  return async dispatch => {
-    const response = await commerce.products.list();
-    dispatch({ type: FETCH_PRODUCTS, payload: response.data });
-  }
+export const fetchProducts = () => async dispatch => {
+  const response = await commerce.products.list();
+  dispatch({ type: FETCH_PRODUCTS, payload: response.data });
+}
+
+export const fetchProduct = id => async dispatch => {
+  const response = await commerce.products.retrieve(id);
+  dispatch({ type: FETCH_PRODUCT, payload: response.data });
 }
